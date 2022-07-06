786c5f51dccce58418c4e1fd49321192effd3e36
Felipe Amaral <felipeamaraldev15@gmail.com>
2022-07-06 14:31:57 -0300

    configurando temas globais

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import theme from './src/global/styles/theme';
 import { Dashboard } from './src/screens/Dashboard';
+import {ThemeProvider} from 'styled-components';
+
 
 export default function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Dashboard />
-    </>
+    </ThemeProvider>
   )
     
 }
