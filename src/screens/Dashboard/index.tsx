296b35697ec9e905d6164571eb734563e2085023
Felipe Amaral <felipeamaraldev15@gmail.com>
2022-07-07 02:21:36 -0300

    construindo a tela de dashboard

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 
-import {Container, Title} from './styles';
+import {Container, Header, UserInfo, Photo, User, UserGreeting, UserName, UserContainer} from './styles';
 
 export function Dashboard() {
     return(
         <Container>
-            <Title>Dashboard</Title>
+            <Header>
+                <UserContainer>
+                    <UserInfo>
+                        <Photo source={{uri: "https://github.com/felipe10amaral.png"}}/>
+                        <User>
+                            <UserGreeting>Olá,</UserGreeting>
+                            <UserName>Felipe</UserName>
+                        </User>
+                    </UserInfo>
+                </UserContainer>    
+            </Header>
         </Container>
     );    
 }
