650c2a3a2575f415bc627705fddfa889d8036ed0
Felipe Amaral <felipeamaraldev15@gmail.com>
2022-07-15 22:26:55 -0300

    criando componente de select

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import { Button } from '../../components/forms/Button';
+import { CategorySelect } from '../../components/forms/CategorySelect';
 import { Input } from '../../components/forms/input';
 import { TransactionTypeButton } from '../../components/forms/TransactionTypeButton';
 import {Container, Header, Title, Form, Fields, TransactionsTypes} from './styles';
@@ -37,7 +38,9 @@ export function Register() {
                     onPress={() => handleTransactionTypeSelect('down')}
                     isActive={transactionType === 'down'}
                 />
-              </TransactionsTypes>    
+              </TransactionsTypes>  
+
+              <CategorySelect title="Categoria" />  
            </Fields> 
             <Button title='Enviar'/>
            
